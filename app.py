329863a5b5f2e3685c247a498e5d79270ae6f43d
Poorna SK <poornashekharkharvi@gmail.com>
2026-05-28 11:48:25 +0530

    AI chatbot project

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, request, jsonify, render_template
+from flask_cors import CORS
+from groq import Groq
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+app = Flask(__name__)
+CORS(app)
+
+client = Groq(
+    api_key=os.getenv("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")
+)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/chat", methods=["POST"])
+def chat():
+
+    user_message = request.json["message"]
+
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": user_message
+            }
+        ],
+        model="llama-3.3-70b-versatile",
+    )
+
+    reply = chat_completion.choices[0].message.content
+
+    return jsonify({
+        "reply": reply
+    })
+
+if __name__ == "__main__":
+    app.run(debug=True)
